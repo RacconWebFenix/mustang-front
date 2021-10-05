a1a0f73ebfaa4cedb62d007f3b296182e66fd66b
Marcos Domingues <dfenixweb@gmail.com>
2021-10-04 22:12:06 -0300

    not cart memory

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div>
       <div className="container">
-        {/* <Cart /> */}
+        <Cart />
       </div>
       <div>
         <div className="cepContainer">
